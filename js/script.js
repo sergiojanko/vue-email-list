3e96906164ea7a2c267b174d4e6fed77763a1d03
sergiojanko <janko.sergio1998@gmail.com>
2021-09-01 15:57:51 +0200

    generate emails

--- js/script.js
+++ js/script.js
@@ -10,8 +10,13 @@ const app = new Vue({
 
     },
     methods: {},
-      created() {
-    axios
-      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    created() {
+        for (let i = 0; i < this.numEmail; i++){
+                axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+            console.log(res);
+            this.emails.push(res.data.response);
+      })
+        }
   },
 });
